95fcb8ecc11cad16f545e1feb3235d44eede1ed1
Tomasz Skoczewski <tomaszskoczewski@gmail.com>
2020-05-07 14:33:47 +0200

    Initial commit

---
+++ src/test/java/com/skoczewski/AppTest.java
@@ -0,0 +1,13 @@
+package com.skoczewski;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testDemo() throws Exception{
+        assertTrue(true);
+    }
+}
