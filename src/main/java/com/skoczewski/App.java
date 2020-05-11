82045af332296780e742bb16cd278b2b6eb5daf1
Tomasz Skoczewski <tomaszskoczewski@gmail.com>
2020-05-11 18:48:17 +0200

    MVN dependencies

--- src/main/java/com/skoczewski/App.java
+++ src/main/java/com/skoczewski/App.java
@@ -1,7 +1,11 @@
 package com.skoczewski;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello");
     }
 }
