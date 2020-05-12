8b41cd477e660e07e46cd1cb82b2c484b8528e50
Tomasz Skoczewski <tomaszskoczewski@gmail.com>
2020-05-12 14:59:37 +0200

    Servlet and Services

---
+++ src/main/java/com/skoczewski/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.skoczewski;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Hello", urlPatterns = {"/api/*"})
+public class HelloServlet extends HttpServlet {
+    private static final String NAME_PARAM = "name";
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private HelloService service = new HelloService();
+
+    /**
+     * Servlet container need it
+     */
+    public HelloServlet() {}
+
+    public HelloServlet(HelloService service) {
+        this.service = service;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Request with parameters: " + req.getParameterMap());
+        resp.getWriter().write(service.prepareGreeting(req.getParameter(NAME_PARAM)));
+
+    }
+}
